6410c2ab0182ecbe2930459bbaca6a0e258257ee
Yago González <yagogonzalezg@gmail.com>
2014-10-27 20:13:12 +0100

    Añadidas clases para almacenar los datos
    La clase ZipAgeStat, superconjunto de AgeGroup, almacena el número de
    pagos y la media por grupo de edades, y todos los grupos de edades según
    la fecha

---
+++ JSON API y/jsonapi_y/tests/AgeGroup.java
@@ -0,0 +1,6 @@
+package tests;
+
+public class AgeGroup {
+	public long numPayments = 0;
+	public double avg = 0;
+}
---
+++ JSON API y/jsonapi_y/tests/ZipAgeStat.java
@@ -0,0 +1,9 @@
+package tests;
+
+import java.util.ArrayList;
+
+public class ZipAgeStat {
+	public String date = null;
+	ArrayList<AgeGroup> ageGroup = new ArrayList<AgeGroup>();
+
+}
